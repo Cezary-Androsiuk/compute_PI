5ed952f5005b81ccda8470fd4711b44f175a95fd
Cezary Androsiuk <czandros@wp.pl>
2022-11-29 13:27:40 +0100

    Finished and working. Refering to previous commits, recursive algorithm was correctly implemented but i wrong write pi function (without power function) and result wasn't correct

--- main.cpp
+++ main.cpp
@@ -1,32 +1,43 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <iomanip>
 
 #define x_0 1
 
 long double pi(const unsigned long long &n);
-long double segment_length(const long double &previous_segment_length, const unsigned long long &recursive_iteratior);
+long double x_n_length(const long double &, const unsigned long long &);
 
 
 int main(int argc, char *argv[]){
+    std::cout << std::setprecision(10000);
     // n-gone == 6*interations
-    unsigned long long iterations = 1;
-    std::cout << "Input PI precision: ";
-    std::cin >> iterations;
-    std::cout << "PI: " << pi(iterations) << std::endl;
+    // for(int i=1; i<100; i++)
+        // std::cout << "PI(" << i << "): " << pi(i) << std::endl;
+    std::cout << "1/3 = " << (long double) 1 / 3 << std::endl << "long double is not perfectly arcurate\n\n";
+    int i = 30;
+    std::cout << "PI(" << i << "): \t" << pi(i) << std::endl;
+    i = 31;
+    std::cout << "PI(" << i << "): \t" << pi(i) << std::endl;
+    i = 1023;
+    std::cout << "PI(" << i << "): \t" << pi(i) << std::endl;
+    i = 1024;
+    std::cout << "PI(" << i << "): \t" << pi(i) << std::endl;
 }
 
 
 long double pi(const unsigned long long &n){
-    return (3 * n * segment_length(x_0,n)); // 3* y(n) * 1 * n
+    // 3* 2^(n-1) * x_n;
+    return 3 * pow(2,n-1) * x_n_length(x_0,n);
+    // return 3 * n * x_n_length(x_0,n);
 }
 
-long double segment_length(const long double &previous_segment_length, const unsigned long long &recursive_iteratior){
-    if(recursive_iteratior == 1)
-        return x_0; // x0 = 1
+long double x_n_length(const long double &x_n_minus_1, const unsigned long long &n){
+    if(n == 1)
+        return x_n_minus_1;
     
-    long double tmp = powl(previous_segment_length/2,2);
-    long double x_n = 1 - sqrtl(1 - tmp);
-    long double new_segnemt_length = sqrtl(powl(1-x_n,2) + tmp);
-    return segment_length(new_segnemt_length, recursive_iteratior-1);
+    long double pow_half_x1 = powl(x_n_minus_1/2, 2);
+    long double h2_top = 1 - sqrtl(1 - pow_half_x1);
+    long double x_n = sqrtl(powl(h2_top,2) + pow_half_x1);
+    return x_n_length(x_n, n-1);
 }
